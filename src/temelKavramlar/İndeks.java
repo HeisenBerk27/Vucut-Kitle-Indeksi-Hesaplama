f863df18135419fe6ab90faab1aa4a63e859fd72
Berk Akgül <108866525+HeisenBerk27@users.noreply.github.com>
2023-02-21 19:44:06 +0300

    Add files via upload

---
+++ src/temelKavramlar/İndeks.java
@@ -0,0 +1,23 @@
+package temelKavramlar;
+
+import java.util.Scanner;
+
+public class İndeks {
+	
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Lütfen boyunuzu metre cinsinden girin: ");
+	        double boy = scanner.nextDouble();
+
+	        System.out.print("Lütfen kilonuzu girin: ");
+	        double kilo = scanner.nextDouble();
+
+	        double vucutKitleIndeksi = kilo / (boy * boy);
+
+	        System.out.printf("Vücut Kitle İndeksiniz: %.2f", vucutKitleIndeksi);
+	    }
+}
+
+
+
